3940b3c52925faa6b22a32220f8c3eedf1257245
Ahmed Nasri <Ahmednasrigeo@gmail.com>
2023-05-31 22:54:17 +0100

    schema built

--- api/dist/product/product.controller.js
+++ api/dist/product/product.controller.js
@@ -15,7 +15,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ProductController = void 0;
 const common_1 = require("@nestjs/common");
 let ProductController = class ProductController {
-    createProduct(name, price, description) { }
+    createProduct(name, price, description) {
+    }
 };
 __decorate([
     (0, common_1.Post)(),
