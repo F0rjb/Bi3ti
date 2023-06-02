4e6278f39cbe75cc3050bbdd061f7129a38c4c80
Ahmed Nasri <Ahmednasrigeo@gmail.com>
2023-06-02 19:39:24 +0100

    form

--- bi3ti/src/features/auth/components/RegistrationForm.componant.tsx
+++ bi3ti/src/features/auth/components/RegistrationForm.componant.tsx
@@ -1,9 +1,11 @@
-
-import { FC, FormEvent } from 'react';
-const RegistrationFormComponent:FC = () => {
-  return (
-    <div></div>
-  )
+import { Box } from '@mui/material';
+import { FC, FormEvent, useReducer } from 'react';
+const RegistrationFormComponent: FC = () => {
+    return (
+        <Box sx={{ border: 1, padding: 2, borderColor: "#cccccc", width: "350px", marginTop: 2 }}>
+            <form>hi</form>
+            </Box>
+    )
 }
 
 export default RegistrationFormComponent
