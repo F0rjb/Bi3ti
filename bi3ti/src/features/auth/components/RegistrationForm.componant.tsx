c225e0997da0153af78348bec5c02645d41f9d07
Ahmed Nasri <Ahmednasrigeo@gmail.com>
2023-06-02 23:39:26 +0100

    register form

--- bi3ti/src/features/auth/components/RegistrationForm.componant.tsx
+++ bi3ti/src/features/auth/components/RegistrationForm.componant.tsx
@@ -1,9 +1,26 @@
-import { Box } from '@mui/material';
+import { Box ,Button,Grid, InputLabel, TextField, Typography} from '@mui/material';
 import { FC, FormEvent, useReducer } from 'react';
 const RegistrationFormComponent: FC = () => {
+   const onSubmitHandler=(e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        console.log("Clicked")
+    }
     return (
         <Box sx={{ border: 1, padding: 2, borderColor: "#cccccc", width: "350px", marginTop: 2 }}>
-            <form>hi</form>
+            <form onSubmit={onSubmitHandler}><Grid container direction="column" justifyContent="flex-start">
+
+                <Typography variant='h4' component="h1">Create Account</Typography>
+                <InputLabel sx={{fontWeight:500 , marginTop:1 ,color:"#000000",}} htmlFor ="name">Your Name</InputLabel>
+                <TextField type='text' name='name' id="name" variant='outlined' size='small' />
+                <InputLabel sx={{fontWeight:500 , marginTop:1 ,color:"#000000",}} htmlFor ="email">Your Email</InputLabel>
+                <TextField type='text' name='email' id="email" variant='outlined' size='small' />
+                <InputLabel sx={{fontWeight:500 , marginTop:1 ,color:"#000000",}} htmlFor ="password">Your Password</InputLabel>
+                <TextField type='text' name='password' id="password" variant='outlined' size='small' placeholder='Minimum 6 characters required ' />
+                <InputLabel sx={{fontWeight:500 , marginTop:1 ,color:"#000000",}} htmlFor ="confirmPassword">Re-enter your Password</InputLabel>
+                <TextField type='text' name='confirmPassword' id="confirmPassword" variant='outlined' size='small' placeholder='Confirm password' />
+                <Button type='submit' variant='contained' style={{marginTop:"16px", height:"31px", backgroundColor : '#f0c14b' , color:"black", borderColor:'#a88734 #9c7e31 #846a29', textTransform:"none"}} >Register</Button>
+                </Grid>
+                </form>
             </Box>
     )
 }
