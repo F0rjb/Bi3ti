de4ab98a03810e8b9c5d593cb366d177210688c4
Ahmed Nasri <Ahmednasrigeo@gmail.com>
2023-06-13 14:48:39 +0100

    fixed cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-  });
+  app.enableCors({ origin: '*' });
   app.setGlobalPrefix('api');
   await app.listen(5000);
 }
